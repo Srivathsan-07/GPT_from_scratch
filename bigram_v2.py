95e94c2b6d9ef7d1069dd4264d2a5b4f22fc234d
Srivathsan_M <srivat7902@gmail.com>
2026-02-11 15:55:29 +0530

    added normalization and scaled the model

--- bigram_v2.py
+++ bigram_v2.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # How many independent sequences will be processed in parallel
-block_size = 8 # What is the maximum context length for predictions?
+batch_size = 64 # How many independent sequences will be processed in parallel
+block_size = 256 # What is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 384 
+n_layer = 6
+n_head = 6
+dropout = 0.2
 # ------------
 
 torch.manual_seed(1337)
@@ -68,6 +71,7 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self,x):
         B,T,C = x.shape 
@@ -89,6 +93,7 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
@@ -105,6 +110,7 @@ class FeedForward(nn.Module):
             nn.Linear(n_embd, 4 *  n_embd),
             nn.ReLU(),
             nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout)
         )
     
     def forward(self, x):
@@ -117,11 +123,13 @@ class Block(nn.Module):
         super().__init__()
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
-        self.ffwd = FeedForward(n_embd)
+        self.ffwd = FeedForward(n_embd) 
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
 
     def forward(self, x):
-        x = x + self.sa(x)
-        x = x + self.ffwd(x)
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
         return x
 
 # super simple bigram model
@@ -132,8 +140,8 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.positional_embeddding_table = nn.Embedding(block_size, n_embd)
-        self.sa_heads = MultiHeadAttention(4,n_embd//4)
-        self.ffwd = FeedForward(n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -142,8 +150,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
         pos_emb = self.positional_embeddding_table(torch.arange(T, device=device)) # (T, C)
         x = tok_emb + pos_emb # (B, T, C)
-        x = self.sa_heads(x) # apply one head of self-attention. (B, T, C)
-        x = self.ffwd(x)
+        x= self.blocks(x) # (B, T, C)
         logits = self.lm_head(x)
 
         if targets is None:
@@ -196,6 +203,9 @@ for iter in range(max_iters):
     optimizer.step()
 
 
+# saving the model
+torch.save(m.state_dict(), 'shakespeare_weights.pt')
+
 # generate from the model
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
 # print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
@@ -203,3 +213,9 @@ context = torch.zeros((1, 1), dtype=torch.long, device=device)
 generated_indices = m.generate(context, max_new_tokens=500)[0].tolist()
 print(f"Generated {len(generated_indices)} tokens.")
 print(decode(generated_indices))
+
+# generating more and saving to a file
+output_text = decode(model.generate(context, max_new_tokens=5000)[0].tolist())
+
+with open('generated_output.txt', 'w', encoding='utf-8') as f:
+    f.write(output_text)
